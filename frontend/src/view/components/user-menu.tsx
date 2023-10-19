d69d6e7174a43cd69f0929cacf396215351b5cfa
Ramon Nunes <ramontsdev@gmail.com>
2023-10-19 15:55:01 -0300

    fix: select none

--- frontend/src/view/components/user-menu.tsx
+++ frontend/src/view/components/user-menu.tsx
@@ -1,16 +1,18 @@
 // import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 
-import { ExitIcon } from "@radix-ui/react-icons";
-import { useAuth } from "../../app/hooks/use-auth";
-import { DropdownMenu } from "./dropdown-menu";
+import { ExitIcon } from '@radix-ui/react-icons';
+
+import { useAuth } from '../../app/hooks/use-auth';
+
+import { DropdownMenu } from './dropdown-menu';
 
 export function UserMenu() {
-  const { signOut, user } = useAuth()
+  const { signOut, user } = useAuth();
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger>
-        <div className="bg-teal-0 rounded-full w-12 h-12 flex items-center justify-center border border-teal-200">
-          <span className="text=sm tracking-[-0.5px] text-teal-900 font-medium">
+        <div className="bg-teal-0 rounded-full w-12 h-12 flex items-center justify-center border border-teal-200 cursor-pointer">
+          <span className="text=sm tracking-[-0.5px] text-teal-900 font-medium select-none">
             {user?.name.slice(0, 2).toLocaleUpperCase()}
           </span>
         </div>
@@ -22,9 +24,9 @@ export function UserMenu() {
           onSelect={signOut}
         >
           Sair
-          <ExitIcon className='w-4 h-4' />
+          <ExitIcon className="w-4 h-4" />
         </DropdownMenu.Item>
       </DropdownMenu.Content>
     </DropdownMenu.Root>
-  )
+  );
 }
